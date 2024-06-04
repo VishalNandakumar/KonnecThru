7d30ad95cfa49a524554a7cfaeca8de73c9feb5a
Sai Valpadasu <67557827+saivalpadasu@users.noreply.github.com>
2024-06-04 13:05:43 -0400

    Revert "Added New Packages and Components"
    This reverts commit 7e1d2dfdec6c44df195979a9e005ff80f7a1cb24.

--- Frontend/konnecthru/src/App.jsx
+++ Frontend/konnecthru/src/App.jsx
@@ -1,11 +1,15 @@
-import "./App.css";
+
+import './App.css'
 
 function App() {
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Konnect Thru</h1>
+      
+      <h1>konnecthru</h1>
+      
     </>
-  );
+  )
 }
 
-export default App;
+export default App
