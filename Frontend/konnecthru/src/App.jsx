c5c6d8c10409a2945d00d1f0e0da6602de88bd18
Sai Valpadasu <67557827+saivalpadasu@users.noreply.github.com>
2024-06-11 01:11:26 -0400

    added material ui

--- Frontend/konnecthru/src/App.jsx
+++ Frontend/konnecthru/src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import Button from "@mui/material/Button";
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Konnect Thru</h1>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
     </>
   );
 }
