afbdef512d256a4361f69fcf9d7fb79d8a46c6a1
Sai Valpadasu <67557827+saivalpadasu@users.noreply.github.com>
2024-06-27 23:31:51 -0400

    logo, color scheme and navbar update
    logo, color scheme and navbar update

--- Frontend/konnecthru/src/App.jsx
+++ Frontend/konnecthru/src/App.jsx
@@ -1,13 +1,10 @@
 import "./App.css";
-import Button from "@mui/material/Button";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Konnect Thru</h1>
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
+      <NavBar />
     </>
   );
 }
