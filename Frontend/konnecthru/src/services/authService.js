fbd41de5622b197f0247a65cfba3fff287fd9998
sunnithdev <sunnithdev@gmail.com>
2024-07-03 17:15:04 -0400

    deployed backend

--- Frontend/konnecthru/src/services/authService.js
+++ Frontend/konnecthru/src/services/authService.js
@@ -5,7 +5,7 @@ import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } f
 // Simulated function to send data to a backend API
 const saveUserDetailsToBackend = async (userId, userDetails) => {
     try {
-        const response = await fetch('http://localhost:5000/api/users/register', {
+        const response = await fetch('https://konnecthru.onrender.com/api/users/register', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
