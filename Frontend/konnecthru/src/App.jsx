17df706fd19294a4d2aef3f67fe5bbbd85f85599
sunnithdev <sunnithdev@gmail.com>
2024-07-22 22:37:08 -0400

    changes

--- Frontend/konnecthru/src/App.jsx
+++ Frontend/konnecthru/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/signup" element={<RegisterPage />} />
           <Route path="/post-a-job" element={<JobPostingForm />} />
           <Route path="/job-listings" element={<ListingPage />} />
           <Route path="/referral-listings" element={<ReferralListingPage />} />
