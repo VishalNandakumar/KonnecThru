c4f83ca6874fb12fdcc1497d24647ea931d1b38e
Sai Valpadasu <67557827+saivalpadasu@users.noreply.github.com>
2024-07-24 18:11:07 -0400

    admin page filtering

--- Frontend/konnecthru/src/pages/ListingPage.jsx
+++ Frontend/konnecthru/src/pages/ListingPage.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import ListingComponent from "../components/ListingComponent";
-import '../index.css'; // Make sure to import your CSS file
+import "../index.css"; // Make sure to import your CSS file
 
 const ListingPage = () => {
   const [jobListings, setJobListings] = useState([]);
@@ -13,17 +13,22 @@ const ListingPage = () => {
       setIsLoading(true);
       setError(null);
       try {
-        const response = await fetch('https://konnecthru.onrender.com/api/jobs/jobpostings');
+        const response = await fetch(
+          "https://konnecthru.onrender.com/api/jobs/jobpostings"
+        );
         if (!response.ok) {
           // This will capture HTTP errors such as 500, 404 etc.
           throw new Error(`HTTP error! status: ${response.status}`);
         }
         const data = await response.json();
-        setJobListings(data);
+        const approvedJobs = data.filter(
+          (job) => job.approvalStatus === "approved"
+        );
+        setJobListings(approvedJobs);
       } catch (error) {
         // Here we capture any network error or one thrown from response status check
         setError(`Failed to fetch jobs: ${error.message}`);
-        console.error('Failed to fetch jobs:', error);
+        console.error("Failed to fetch jobs:", error);
       } finally {
         setIsLoading(false);
       }
@@ -46,10 +51,16 @@ const ListingPage = () => {
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 flex flex-col md:flex-row">
           <aside className="md:w-1/4 bg-white p-4 rounded-lg shadow-lg mb-6 md:mb-0 md:mr-6">
             <h2 className="text-xl font-bold mb-4">Filters and Sort</h2>
-            <div className="custom-flex-space">
-              <button className="px-3 py-1 bg-red-500 text-white rounded-full">Jobs</button>
-              <button className="px-3 py-1 bg-gray-200 text-gray-700 rounded-full">Interns</button>
-              <button className="px-3 py-1 bg-gray-200 text-gray-700 rounded-full">Alerts</button>
+            <div className="flex space-x-4">
+              <button className="px-3 py-1 bg-red-500 text-white rounded-full">
+                Jobs
+              </button>
+              <button className="px-3 py-1 bg-gray-200 text-gray-700 rounded-full">
+                Interns
+              </button>
+              <button className="px-3 py-1 bg-gray-200 text-gray-700 rounded-full">
+                Alerts
+              </button>
             </div>
           </aside>
           <section className="flex-1 bg-white p-4 rounded-lg shadow-lg">
